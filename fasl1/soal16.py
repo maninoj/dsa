52305d3dda68c17dc4e4a09e09489a6994104103
maninoj <maninajafi1384@gmail.com>
2024-10-16 14:11:08 +0330

    soal shanzdahom

---
+++ fasl1/soal16.py
@@ -0,0 +1,11 @@
+def generate_sub(arr, index=0, curr_sub=[]):
+    print(curr_sub)
+
+    for i in range(index, len(arr)):
+        curr_sub.append(arr[i])
+        generate_sub(arr, index+1, curr_sub)
+        curr_sub.pop()
+
+
+if __name__ == '__main__':
+    generate_sub([1, 2, 3])
