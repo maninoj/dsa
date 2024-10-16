13496cdda43c30dd54ce9bc6cc1a4f0e9301e1cd
maninoj <maninajafi1384@gmail.com>
2024-10-16 11:44:30 +0330

    soal dahom

---
+++ fasl1/soal10.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+
+    return n * factorial(n-1)
+
+
+def reverse_sum_fact(n):
+    if n == 0:
+        return 0
+
+    else:
+        return 1/factorial(n) + reverse_sum_fact(n-1)
