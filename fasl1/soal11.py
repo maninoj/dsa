77e1a2fb97ef7f3ed2af5b6df9c6713a9a90350b
maninoj <maninajafi1384@gmail.com>
2024-10-16 11:51:49 +0330

    soal yazdahom

---
+++ fasl1/soal11.py
@@ -0,0 +1,17 @@
+def coin_combination(n, coins, current_comb=[]):
+    if n == 0:
+        print(current_comb)
+        return
+    if n < 0:
+        return
+
+    for coin in coins:
+        coin_combination(n-coin, coins, current_comb+[coin])
+
+
+if __name__ == '__main__':
+    coins = [2, 5, 10]
+    n = 10
+
+    coin_combination(n, coins)
+
