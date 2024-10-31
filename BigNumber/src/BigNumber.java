90ff239f82b8753a7ef898c419f05dde03653970
maninoj <maninajafi1384@gmail.com>
2024-10-31 11:34:05 +0330

    make empty func

--- BigNumber/src/BigNumber.java
+++ BigNumber/src/BigNumber.java
@@ -1,2 +1,26 @@
 public class BigNumber {
+    private byte[] digit;
+    private boolean sign;
+
+    public BigNumber(){}
+
+    public BigNumber(String bn){}
+
+    public BigNumber(int[] a){}
+
+    public BigNumber(byte[] a){}
+
+    public BigNumber(int a){}
+
+    public BigNumber add(BigNumber bn){}
+
+    public BigNumber subtract(BigNumber bn){}
+
+    public BigNumber multiplyByOne(int n){}
+
+    public BigNumber increment(){}
+
+    public BigNumber decrement(){}
+    
+
 }
