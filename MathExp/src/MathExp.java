2b495194833127f2a10c26b87756b803026c2f0d
maninoj <maninajafi1384@gmail.com>
2024-12-06 20:24:37 +0330

    create Main class

--- MathExp/src/MathExp.java
+++ MathExp/src/MathExp.java
@@ -304,18 +304,3 @@ public class MathExp {
 
 
 }
-
-
-class main{
-    public static void main(String[] args){
-
-        MathExp a = new MathExp();
-        // System.out.println(a.toPostFix());
-        // System.out.println(a.toPreFix());
-        // System.out.println(a.calculatePostfix());
-        // System.out.println(a.calculate(3));
-        a.graph();
-
-        // draw.paint();
-    }
-}
