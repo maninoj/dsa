fb033f193714b97b212740f69f59032492c2aa86
maninoj <maninajafi1384@gmail.com>
2025-01-01 16:47:30 +0330

    soal 8 fasl4 done

---
+++ fasl7/src/soal8.java
@@ -0,0 +1,12 @@
+public class soal8 {
+    public soal8(TwoLinkNode head){
+        TwoLinkNode tail = head;
+
+        while(tail.next != null){
+            tail = tail.next;
+        }
+
+        tail.next = head;
+        head.prev = tail;
+    }
+}
